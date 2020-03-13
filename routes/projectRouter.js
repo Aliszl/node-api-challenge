ad357c0db335c73b3c6d54c0e0c70901e0f023c7
Alison Law <alisparklaw@gmail.com>
2020-03-13 10:12:26 +0000

    delete functional

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -28,12 +28,37 @@ router.post("/api", async (req, res) => {
         error: "could not add project"
       });
     });
-}); //end
+});
 
 router.get("/api/:id", async (req, res) => {
   const { id } = req.params;
   const projects = await helpers.get(id);
   res.status(200).json({ projects });
 });
+
+router.delete('/api/:id', async(req, res) => {
+    const { id } = req.params;
+    const deletedProject =[];
+  
+    helpers.get(id).then(project =>{
+      deletedProjects.push(project);
+      res.status(200).json(project);
+    })
+  
+    helpers
+      .remove(id)
+      .then(project => {
+        if (!project) {
+          res
+            .status(404)
+            .json({ message: "The project with the specified ID does not exist." });
+        } else {
+          res.status(204).json({ message: "Removed " });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
 
 module.exports = router;
