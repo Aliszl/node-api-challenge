df87af6f85d14cbd462c833837f9e9d0892c983d
Alison Law <alisparklaw@gmail.com>
2020-03-13 10:43:23 +0000

    post action to project functional

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -3,8 +3,37 @@ const helpers = require("../data/helpers/actionModel")
 
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     res.json({ message: 'The challenge api is UP' })
-//    })
+router.get('/', (req, res) => {
+    res.json({ message: 'The challenge api is UP' })
+   })
 
+// router.get("/api/:id", async (req, res) => {
+//     const { id } = req.params;
+//     const actions = await helpers.get(id);
+//     res.status(200).json({ actions });
+//   });
+  
+
+router.post("/api/:id", async (req, res) => {
+    const payload = req.body;
+    helpers
+      .insert(payload)
+      .then(action => {
+        if (!action) {
+          res.staus(400).json({
+            errorMessage: "Please provide description and notes"
+          });
+        } else {
+          res.status(201).json(payload);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.end();
+        res.status(500).json({
+          error: "could not add action"
+        });
+      });
+  });
+  
 module.exports = router;
